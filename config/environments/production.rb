ddac50415e7ef2e4b020ac9834a814b91c3acb96
Ramanujan Jan <esis_coder@libero.it>
2012-06-20 21:49:22 +0200

    config.serve_static_assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,8 @@ Elyts::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # (DOM RETURN TO FALSE) Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
